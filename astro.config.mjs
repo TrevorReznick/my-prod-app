2be7c3041a6e923513724a2e35cb42ef2525f272
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2024-06-03 12:54:06 +0200

    test new login routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind'
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
-  output: 'server'
-});
+  site: 'https://www.xdomain.it',
+  integrations: [
+    vue(),
+    tailwind()
+  ],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+})
