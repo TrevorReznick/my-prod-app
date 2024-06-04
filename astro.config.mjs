8cb4bb5695ba9249f81306957dbb6f0ba15aa71d
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2024-06-04 09:17:45 +0200

    netifly settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
-import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.xdomain.it',
-  integrations: [
-    vue(),
-    cloudflare()
-  ],
+  integrations: [vue(), cloudflare()],
   output: 'server',
-  adapter: cloudflare()
-})
+  adapter: netlify()
+});
