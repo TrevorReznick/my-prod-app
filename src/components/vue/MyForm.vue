a6ce163a33729e24d22cfe3e4b126d62ca8c18f6
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2024-06-04 11:47:13 +0200

    add vue test files

---
+++ src/components/vue/MyForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div class="container">
+      <h1 class="text-3xl font-bold mb-4">Autenticazione</h1>
+      <div class="form-container border-2 border-purple-400 p-4 rounded-lg">
+        <form>
+          <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+          </div>
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+          </div>
+          <button type="submit">Accedi</button>
+        </form>
+      </div>
+    </div> 
+  </section>
+</template>
+
+<style scoped>
+
+</style>
+    
+
+    
