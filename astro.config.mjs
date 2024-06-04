2c6fc54f57c3b88125fbd9f7f09f0e8dab011f78
Tuo Nome <double.facessss@gmail.com>
2024-06-04 08:15:49 +0200

    test fetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind'
 
 
@@ -9,10 +9,8 @@ export default defineConfig({
   site: 'https://www.xdomain.it',
   integrations: [
     vue(),
-    tailwind()
+    cloudflare()
   ],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare()
 })
---
+++ src/pages/api/v1/main-category.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from 'astro'
+import { supabase } from '../../../providers/supabase'
+
+export const GET: APIRoute = async () => {
+  const { data, error } = await supabase
+    .from("main_category")
+    .select("*")
+    .order("id", { ascending: true })
+
+  if (error) {
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      { status: 500 },
+    );
+  }
+
+  return new Response(JSON.stringify(data))
+};
+
