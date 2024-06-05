e06cacf0ca07b1007b76f71ce2d87a9d5a04937b
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2024-06-05 09:05:23 +0200

    setting api url

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly PROD_API_URL: string;
+    readonly DEV_API_URL: string;
+    // Aggiungi altre variabili d'ambiente qui, se necessario
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
