54f1a1a65e172e945962aec81fb1b3659afecb5a
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2024-07-08 12:36:49 +0200

    Updated info.ts

--- src/pages/api/v1/info.ts
+++ src/pages/api/v1/info.ts
@@ -1,8 +1,6 @@
 import type { APIRoute } from 'astro'
 
-/*PUBLIC_THUMBNAIL_WS_API_KEY*/
-
-const thumbnail_ws_api_key = import.meta.env.PUBLIC_THUMBNAIL_WS_API_KEY
+//const thumbnail_ws_api_key = import.meta.env.PUBLIC_THUMBNAIL_WS_API_KEY
 
 export const GET: APIRoute = async (request) => {
 
@@ -10,30 +8,9 @@ export const GET: APIRoute = async (request) => {
 
     
     const url = new URL(request.url)
-    console.log('url', url)
+    //console.log('url', url)
     const siteUrl = url.searchParams.get('url')
-    console.log('siteUrl', siteUrl)
-    /*
-
-    const url = 'https://url-screenshot-api1.p.rapidapi.com/screenshot?url=https%3A%2F%2Fexample.com%2F'
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': '379bce5eb3msh4d699d2b4e5eadcp156716jsnad3b8f073b87',
-            'x-rapidapi-host': 'url-screenshot-api1.p.rapidapi.com'
-        }
-    }
-
-    try {
-        const response = await fetch(url, options)
-        const result = await response.text()
-        console.log(result)
-    } catch (error) {
-        console.error(error)
-    }
-
-    */
-
+    //console.log('siteUrl', siteUrl)
     if (!siteUrl) {
         return new Response(
             JSON.stringify({
@@ -48,21 +25,32 @@ export const GET: APIRoute = async (request) => {
 
     try {
         
-        const response = await fetch(`https://api.thumbnail.ws/api/${thumbnail_ws_api_key}/thumbnail/get?url=${encodeURIComponent(siteUrl)}&width=200`)        
-        
+        //const response = await fetch(`https://api.thumbnail.ws/api/${thumbnail_ws_api_key}/thumbnail/get?url=${encodeURIComponent(siteUrl)}&width=200`)
+        const url = 'https://url-screenshot-api1.p.rapidapi.com/screenshot?url=https%3A%2F%2Fexample.com%2F'
+        const options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '379bce5eb3msh4d699d2b4e5eadcp156716jsnad3b8f073b87',
+                'x-rapidapi-host': 'url-screenshot-api1.p.rapidapi.com'
+            }
+        }
         if (!response.ok) {
             throw new Error('Failed to fetch data')
         }
-        const data = await response.json()
+
+        const response = await fetch(url, options)
+        const result = await response.text()
+        console.log(result)        
+        
 
         return new Response(
             JSON.stringify({
                 message: 'Welcome to our API!',
-                data: data,
+                data: result,
             }),
             {
                 status: 200,
-                headers: { 'Content-Type': 'application/json' },
+                headers: { 'Content-Type': 'application/jpeg' },
             }
         )
     } catch (error: any) {
