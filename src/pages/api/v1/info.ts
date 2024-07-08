7f9f5dc3b2a72f8025bd29e6d2569b5be85f9eb7
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2024-07-08 11:24:51 +0200

    Test RapidApi apis

--- src/pages/api/v1/info.ts
+++ src/pages/api/v1/info.ts
@@ -13,6 +13,25 @@ export const GET: APIRoute = async (request) => {
     const siteUrl = url.searchParams.get('url')
     console.log('siteUrl', siteUrl)
 
+    const url = 'https://url-screenshot-api1.p.rapidapi.com/screenshot?url=https%3A%2F%2Fexample.com%2F'
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '379bce5eb3msh4d699d2b4e5eadcp156716jsnad3b8f073b87',
+            'x-rapidapi-host': 'url-screenshot-api1.p.rapidapi.com'
+        }
+    }
+
+    try {
+        const response = await fetch(url, options)
+        const result = await response.text()
+        console.log(result)
+    } catch (error) {
+        console.error(error)
+    }
+
+    /*
+
     if (!siteUrl) {
         return new Response(
             JSON.stringify({
@@ -55,5 +74,6 @@ export const GET: APIRoute = async (request) => {
             }
         )
     }
+    */
 }
 
